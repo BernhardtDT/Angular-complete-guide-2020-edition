f99b9fd22123c7a4760c0cf76ab1461a3d7e3e94
Bernhardt du toit <Bernhardt.bdt@gmail.com>
2020-09-28 17:06:08 +0200

    NgRx

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -1,12 +1,13 @@
-import { Action } from "@ngrx/store";
-import { Ingredient } from "src/app/shared/ingredient.model";
+import { Action } from '@ngrx/store';
 
-export const ADD_INGREDIENT = "[Shopping List] Add Ingredient";
-export const ADD_INGREDIENTS = "[Shopping List] Add Ingredients";
-export const UPDATE_INGREDIENT = "[Shopping List] Update Ingredient";
-export const DELETE_INGREDIENT = "[Shopping List] Delete Ingredient";
-export const START_EDIT = "[Shopping List] Start Edit";
-export const STOP_EDIT = "[Shopping List] Stop Edit";
+import { Ingredient } from '../../shared/ingredient.model';
+
+export const ADD_INGREDIENT = '[Shopping List] Add Ingredient';
+export const ADD_INGREDIENTS = '[Shopping List] Add Ingredients';
+export const UPDATE_INGREDIENT = '[Shopping List] Update Ingredient';
+export const DELETE_INGREDIENT = '[Shopping List] Delete Ingredient';
+export const START_EDIT = '[Shopping List] Start Edit';
+export const STOP_EDIT = '[Shopping List] Stop Edit';
 
 export class AddIngredient implements Action {
   readonly type = ADD_INGREDIENT;
